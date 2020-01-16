1758e277df04b2d9a89b74b8b01bf462d97d33a1
ericknishio <erickknishio@gmail.com>
2020-01-16 17:05:03 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,6 +29,11 @@ public class Program {
 		for (Seller sel : list) { //Para cada seller sel na lista
 			System.out.println(sel); //Imprimir o sel	
 		}
+		
+		System.out.println("\n==== TEST 4: sellet insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New ID = " + newSeller.getId());
 	}
 
 }
